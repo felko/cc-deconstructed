9cb7796b849cdf7f86786867a1d592141b20adfa
felko <baxbaxwalanuksiwe@gmail.com>
2023-12-14 09:42:34 +0100

    update toolchain, add leandojo

--- lakefile.lean
+++ lakefile.lean
@@ -3,11 +3,18 @@ open System Lake DSL
 
 package ccdeconstructed
 
+require std from git
+  "https://github.com/leanprover/std4" @ "v4.3.0"
+
+require LeanCopilot from git
+  "https://github.com/lean-dojo/LeanCopilot.git" @ "v1.0.0"
+
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4"@"v4.3.0-rc2"
+  "https://github.com/leanprover-community/mathlib4"@"v4.3.0"
 
 require Qq from git
   "https://github.com/leanprover-community/quote4"
 
 @[default_target]
-lean_lib CCDeconstructed
+lean_lib CCDeconstructed where
+  moreLinkArgs := #[s!"-L{__dir__}/.lake/build/lib", "-lctranslate2"]
